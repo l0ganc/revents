9bf3db52a2b6cfea4775bbe4361aeed53e5051b1
l0ganc <jingyige@usc.edu>
2018-11-07 09:12:28 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let render = () => {
+  ReactDOM.render(<App />, rootEl)
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render)
+  })
+}
+
+render()
+
+registerServiceWorker();
